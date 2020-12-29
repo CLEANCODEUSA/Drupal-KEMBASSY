694ac71249629b070434ad2bace1cb6dfde152be
PHPRHiNOS <vanbeek@drupalsailor.com>
2020-12-29 12:16:21 -0500

    Update scripts.js

--- themes/custom/kembassy/js/scripts.js
+++ themes/custom/kembassy/js/scripts.js
@@ -4,5 +4,16 @@
  * and open the template in the editor.
  */
 jQuery(document).ready(function(){
-    console.log('loading scripts.js');
+    var title = jQuery('.section-2 h2.color-title').html();
+    var color_title = '';
+    
+    for(var i = 0; i < title.length; i++){
+        if(i != 2){
+            color_title += title[i];
+        }
+        else{
+            color_title += "<span class='pink'>" + title[i] + "</span>";
+        }
+    }
+    jQuery('.section-2 h2.color-title').html(color_title);
 });
