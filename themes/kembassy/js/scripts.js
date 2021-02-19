a4a818ee09e56e5a2b789380158dcac28bd0ff69
PHPRHiNOS <vanbeek@drupalsailor.com>
2021-02-19 16:20:25 -0500

    Update scripts.js

--- themes/kembassy/js/scripts.js
+++ themes/kembassy/js/scripts.js
@@ -1,15 +1,18 @@
 jQuery(document).ready(function () {
   var title = jQuery('.section-2 h2.color-title').html();
-  var color_title = '';
 
-  for (var i = 0; i < title.length; i++) {
-    if (i != 2) {
-      color_title += title[i];
-    } else {
-      color_title += "<span class='pink'>" + title[i] + "</span>";
+  if (title) {
+    var color_title = '';
+
+    for (var i = 0; i < title.length; i++) {
+      if (i != 2) {
+        color_title += title[i];
+      } else {
+        color_title += "<span class='pink'>" + title[i] + "</span>";
+      }
     }
+    jQuery('.section-2 h2.color-title').html(color_title);
   }
-  jQuery('.section-2 h2.color-title').html(color_title);
 });
 
 // Add smooth scrolling to all links
